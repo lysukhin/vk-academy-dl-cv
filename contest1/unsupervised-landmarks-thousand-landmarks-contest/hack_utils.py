b0e0c22ef8f3ebf7332c38dcd50530c4cba35d64
lysukhin <mr.lysuhin@gmail.com>
2020-04-16 16:30:18 +0300

    Added csv casting dtypes

--- contest1/unsupervised-landmarks-thousand-landmarks-contest/hack_utils.py
+++ contest1/unsupervised-landmarks-thousand-landmarks-contest/hack_utils.py
@@ -13,6 +13,9 @@ TRAIN_SIZE = 0.8
 NUM_PTS = 971
 CROP_SIZE = 128
 SUBMISSION_HEADER = "file_name,Point_M0_X,Point_M0_Y,Point_M1_X,Point_M1_Y,Point_M2_X,Point_M2_Y,Point_M3_X,Point_M3_Y,Point_M4_X,Point_M4_Y,Point_M5_X,Point_M5_Y,Point_M6_X,Point_M6_Y,Point_M7_X,Point_M7_Y,Point_M8_X,Point_M8_Y,Point_M9_X,Point_M9_Y,Point_M10_X,Point_M10_Y,Point_M11_X,Point_M11_Y,Point_M12_X,Point_M12_Y,Point_M13_X,Point_M13_Y,Point_M14_X,Point_M14_Y,Point_M15_X,Point_M15_Y,Point_M16_X,Point_M16_Y,Point_M17_X,Point_M17_Y,Point_M18_X,Point_M18_Y,Point_M19_X,Point_M19_Y,Point_M20_X,Point_M20_Y,Point_M21_X,Point_M21_Y,Point_M22_X,Point_M22_Y,Point_M23_X,Point_M23_Y,Point_M24_X,Point_M24_Y,Point_M25_X,Point_M25_Y,Point_M26_X,Point_M26_Y,Point_M27_X,Point_M27_Y,Point_M28_X,Point_M28_Y,Point_M29_X,Point_M29_Y\n"
+DTYPES = {"file_name": str}
+for header in SUBMISSION_HEADER.strip().split(",")[1:]:
+    DTYPES[header] = np.int32
 
 
 class ScaleMinSideToSize(object):
@@ -81,7 +84,7 @@ class ThousandLandmarksDataset(data.Dataset):
             else os.path.join(root, "test_points.csv")
         images_root = os.path.join(root, "images")
 
-        df = pd.read_csv(landmark_file_name, delimiter='\t')
+        df = pd.read_csv(landmark_file_name, delimiter='\t', dtype=DTYPES)
 
         self.landmarks = []
         self.image_names = []
