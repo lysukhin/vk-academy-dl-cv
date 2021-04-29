a0f9093722fec7d34566c454204386d5cbfe2843
d.lysukhin <d.lysukhin@corp.mail.ru>
2021-04-29 11:01:14 +0300

    typo

--- contest02/scripts/train-regression.py
+++ contest02/scripts/train-regression.py
@@ -1,3 +1,3 @@
 # TODO TIP: Probably, to be more effective at recognition stage, one should learn how to extract license plates
-# more accurate than roughly cropping the bounding box.
+# more accurately than roughly cropping the bounding box.
 # Key words: 'landmarks'.
