f74322585202d5407453c3623655d844621aa4b1
Ivan Karpukhin <i.a.karpukhin@tinkoff.ru>
2022-04-22 19:21:56 +0300

    update download

--- computer_vision/seminar06-segmentation/download.sh
+++ computer_vision/seminar06-segmentation/download.sh
@@ -1,4 +1,24 @@
 export URL='https://raw.githubusercontent.com/lysukhin/MADE/2022/computer_vision/seminar06-segmentation/'
 wget "$URL/augmentations.py"
 wget "$URL/seminar.py"
-wget --recursive "$URL/resources"
+mkdir resources
+wget "$URL/resources/example-image.jpg" -O resources/example-image.jpg
+wget "$URL/resources/unet-window.jpg" -O resources/unet-window.jpg
+wget "$URL/resources/00-1-noaug.jpg" -O resources/00-1-noaug.jpg
+wget "$URL/resources/02-pretrained.jpg" -O resources/02-pretrained.jpg
+wget "$URL/resources/01-1-aug.jpg" -O resources/01-1-aug.jpg
+wget "$URL/resources/00-2-noaug.jpg" -O resources/00-2-noaug.jpg
+wget "$URL/resources/made.jpg" -O resources/made.jpg
+wget "$URL/resources/example-mask.jpg" -O resources/example-mask.jpg
+wget "$URL/resources/00-3-noaug.jpg" -O resources/00-3-noaug.jpg
+wget "$URL/resources/u-net.jpg" -O resources/u-net.jpg
+mkdir cvmade
+wget "$URL/cvmade" -O cvmade
+wget "$URL/cvmade/plot" -O cvmade/plot
+wget "$URL/cvmade/plot/plot.py" -O cvmade/plot/plot.py
+wget "$URL/cvmade/plot/__init__.py" -O cvmade/plot/__init__.py
+wget "$URL/cvmade/plot/torch.py" -O cvmade/plot/torch.py
+wget "$URL/cvmade/__init__.py" -O cvmade/__init__.py
+wget "$URL/cvmade/torch.py" -O cvmade/torch.py
+wget "$URL/cvmade/image.py" -O cvmade/image.py
+
