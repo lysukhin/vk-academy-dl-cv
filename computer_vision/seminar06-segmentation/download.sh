e8f718798029455179a0cc1cf8fcc930e38ac216
Ivan Karpukhin <i.a.karpukhin@tinkoff.ru>
2022-04-22 19:46:55 +0300

    fix download.sh

--- computer_vision/seminar06-segmentation/download.sh
+++ computer_vision/seminar06-segmentation/download.sh
@@ -1,7 +1,7 @@
 export URL='https://raw.githubusercontent.com/lysukhin/MADE/2022/computer_vision/seminar06-segmentation/'
 wget "$URL/augmentations.py"
 wget "$URL/seminar.py"
-mkdir resources
+mkdir -p resources
 wget "$URL/resources/example-image.jpg" -O resources/example-image.jpg
 wget "$URL/resources/unet-window.jpg" -O resources/unet-window.jpg
 wget "$URL/resources/00-1-noaug.jpg" -O resources/00-1-noaug.jpg
@@ -12,9 +12,8 @@ wget "$URL/resources/made.jpg" -O resources/made.jpg
 wget "$URL/resources/example-mask.jpg" -O resources/example-mask.jpg
 wget "$URL/resources/00-3-noaug.jpg" -O resources/00-3-noaug.jpg
 wget "$URL/resources/u-net.jpg" -O resources/u-net.jpg
-mkdir cvmade
-mkdir cvmade/plot
-wget "$URL/cvmade" -O cvmade
+mkdir -p cvmade
+mkdir -p cvmade/plot
 wget "$URL/cvmade/plot/plot.py" -O cvmade/plot/plot.py
 wget "$URL/cvmade/plot/__init__.py" -O cvmade/plot/__init__.py
 wget "$URL/cvmade/plot/torch.py" -O cvmade/plot/torch.py
